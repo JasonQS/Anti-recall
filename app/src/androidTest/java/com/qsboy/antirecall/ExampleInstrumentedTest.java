5bbde950f584cdf9dc7d353a36fc0cf6b465918a
jason <qiansheng1998@gmial.com>
2017-10-18 20:14:19 +0800

    commit

--- app/src/androidTest/java/com/qsboy/antirecall/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/qsboy/antirecall/ExampleInstrumentedTest.java
@@ -1,13 +1,18 @@
 package com.qsboy.antirecall;
 
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
+
+import com.qsboy.antirecall.sql.DBHelper;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static com.qsboy.antirecall.sql.DBHelper.TABLE_NAME;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumentation test, which will execute on an Android device.
@@ -22,5 +27,13 @@ public class ExampleInstrumentedTest {
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.qsboy.antirecall", appContext.getPackageName());
+
+        SQLiteDatabase db;
+        SQLiteOpenHelper helper = new DBHelper(appContext, DBHelper.DB_NAME, null, DBHelper.DB_VERSION);
+        db = helper.getWritableDatabase();
+        db.beginTransaction();
+        String sql = "create table if not exists " + TABLE_NAME + " (Id integer primary key, Withdrawals text, Name text, Time text)";
+        db.execSQL(sql);
+
     }
 }
