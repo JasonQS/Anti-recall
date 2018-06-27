d31e8789e14463ffa1fc6fffe78c5a51c9f1c1d9
JasonQS <qiansheng1998@gmail.com>
2018-06-28 00:43:50 +0800

    update

---
+++ app/src/main/java/com/qsboy/antirecall/ui/HorizontalPagerFragment.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright © 2016 - 2018 by GitHub.com/JasonQS
+ * anti-recall.qsboy.com
+ * All Rights Reserved
+ */
+
+package com.qsboy.antirecall.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.gigamole.infinitecycleviewpager.HorizontalInfiniteCycleViewPager;
+import com.qsboy.antirecall.R;
+
+/**
+ * Created by GIGAMOLE on 8/18/16.
+ */
+public class HorizontalPagerFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(final LayoutInflater inflater, @Nullable final ViewGroup container, @Nullable final Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_price, container, false);
+    }
+
+    @Override
+    public void onViewCreated(final View view, @Nullable final Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        final HorizontalInfiniteCycleViewPager horizontalInfiniteCycleViewPager = view.findViewById(R.id.horizontalInfiniteCycleViewPager);
+        horizontalInfiniteCycleViewPager.setAdapter(new HorizontalPagerAdapter(getContext(), false));
+
+//        horizontalInfiniteCycleViewPager.setScrollDuration(400);
+//        horizontalInfiniteCycleViewPager.setPageDuration(1000);
+//        horizontalInfiniteCycleViewPager.setInterpolator(
+//                AnimationUtils.loadInterpolator(getContext(), android.R.anim.overshoot_interpolator)
+//        );
+//        horizontalInfiniteCycleViewPager.setMediumScaled(false);
+//        horizontalInfiniteCycleViewPager.setMaxPageScale(0.8F);
+//        horizontalInfiniteCycleViewPager.setMinPageScale(0.5F);
+//        horizontalInfiniteCycleViewPager.setCenterPageScaleOffset(30.0F);
+//        horizontalInfiniteCycleViewPager.setMinPageScaleOffset(5.0F);
+//        horizontalInfiniteCycleViewPager.setOnInfiniteCyclePageTransformListener();
+
+//        horizontalInfiniteCycleViewPager.setCurrentItem(
+//                horizontalInfiniteCycleViewPager.getRealItem() + 1
+//        );
+    }
+}
---
+++ app/src/main/java/com/qsboy/antirecall/ui/PriceFragment.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright © 2016 - 2018 by GitHub.com/JasonQS
+ * anti-recall.qsboy.com
+ * All Rights Reserved
+ */
+
+package com.qsboy.antirecall.ui;
+
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.gigamole.infinitecycleviewpager.HorizontalInfiniteCycleViewPager;
+import com.qsboy.antirecall.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class PriceFragment extends Fragment {
+
+
+    public PriceFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_price, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        final HorizontalInfiniteCycleViewPager horizontalInfiniteCycleViewPager = view.findViewById(R.id.horizontalInfiniteCycleViewPager);
+        horizontalInfiniteCycleViewPager.setAdapter(new HorizontalPagerAdapter(getContext(), false));
+
+//        horizontalInfiniteCycleViewPager.setScrollDuration(400);
+//        horizontalInfiniteCycleViewPager.setPageDuration(1000);
+//        horizontalInfiniteCycleViewPager.setInterpolator(
+//                AnimationUtils.loadInterpolator(getContext(), android.R.anim.overshoot_interpolator)
+//        );
+//        horizontalInfiniteCycleViewPager.setMediumScaled(false);
+//        horizontalInfiniteCycleViewPager.setMaxPageScale(0.8F);
+//        horizontalInfiniteCycleViewPager.setMinPageScale(0.5F);
+//        horizontalInfiniteCycleViewPager.setCenterPageScaleOffset(30.0F);
+//        horizontalInfiniteCycleViewPager.setMinPageScaleOffset(5.0F);
+//        horizontalInfiniteCycleViewPager.setOnInfiniteCyclePageTransformListener();
+
+//        horizontalInfiniteCycleViewPager.setCurrentItem(
+//                horizontalInfiniteCycleViewPager.getRealItem() + 1
+//        );
+
+    }
+}
